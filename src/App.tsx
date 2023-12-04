f8d4229f569c10e1ee84183ad100945b9c23ba04
Vladislav <labkovich.vl@gmail.com>
2023-12-05 00:37:02 +0300

    dev branch commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,6 +7,7 @@ function App() {
           <div>
             <div>App</div>
             <div>Dev branch commit 1</div>
+              <div>Dev branch commit 2</div>
           </div>
         </div>
     );
